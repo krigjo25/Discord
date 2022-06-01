d65e226983bf3cf51bf626b51ce53459578a3f73
krigjo25 <krigjo25@gmail.com>
2022-06-01 08:59:10 +0200

    Update pyChat.py

--- pyChat/pylib/demochatbot/pyChat.py
+++ pyChat/pylib/demochatbot/pyChat.py
@@ -1,4 +1,5 @@
 import os
+import transformers
 import numpy as np
 import speech_recognition as sr
 
@@ -8,70 +9,74 @@ from gtts import gTTS
 class PyChat():
 
     def __init__(self, name):
+
         self.name = name
         print(f'--- Starting up {name} ---')
-        return
-    @staticmethod
 
+        return
+
+    @staticmethod
     def TextSpeech(text):
 
         speaker = gTTS(text=text, lang='en', slow=False)
-        speaker.save('/records/res.mp3')
-        os.system("start/records/res.mp3")
-        os.remove('/records/res.mp3')
-        print('{self.name} : {text}')
+        speaker.save('res.mp3')
+        os.system('start res.mp3')
+        os.remove('res.mp3')
+        print(f'AI :> {text}')
 
         return
 
-    def WakePyChatUp(self, text):
-        return True if self.name in text.lower() else False
+    @staticmethod
+    def AiDate():
+
+        now = datetime.now().date().strftime('%d. %b, %Y')
+
+        return f' Today\'s date {now}'
 
     @staticmethod
-    def AiClock(text):
+    def AiTime():
 
         now = datetime.now().time().strftime('%H:%M')
-        return f'It\'s {now} a Clock'
-    
-    def AiDate(self):
 
-        date = datetime.date().strftime('%d/%a-%y')
-        return f'Today\'s date : {date}'
-        
+        return f'It\'s {now} a Clock.'
+
     def SpeechToText(self):
 
         reconizer = sr.Recognizer()
 
         with sr.Microphone() as mic:
-            print('Listening to your voice')
+            print('Listening to your voice...')
             audio = reconizer.listen(mic)
 
         try:
             self.text = reconizer.recognize_google(audio)
-            print(f'me > {self.text}')
+            print(f'me -> {self.text}')
 
         except:
-            print('me got an error')
+            print('I\'m sorry Something went south')
 
         return 
-    
+
+    def WakePyChatUp(self, text):
+        return True if self.name in text else False
+
 if __name__ == '__main__':
 
-    ai = PyChat(name = 'Julie')
+    ex = True
+    ai = PyChat(name = 'Jake')
+    npl = transformers.pipelines('conversational', model='microsoft/DialoGPT-medium')
+    os.environ['TOKENIZERS_PARALLELISM'] = True
 
-    while True:
+    while ex:
 
         ai.SpeechToText()
 
-        if ai.WakePyChatUp(ai.text) is True:
-            
-            res = 'Hello, I\'m PyChat, What can i do for you?'
-            ai.TextSpeech(res)
-
-            #   Action to be done
-        elif "time" in ai.text:
-            res= ai.AiClock()
-        else:
-            res = 'I\'m sorry, Come again?'
-            ai.TextSpeech(res)
-
-    print(f'---  Closing down {self.name}')
+        if ai.WakePyChatUp(ai.text) is True:res = 'Hello, I\'m PyChat, What can i do for you?'
+        elif 'time' in ai.text:res = ai.AiTime()
+        elif 'date' in ai.text:res = ai.AiDate()
+        elif any(i in ai.text for i in ['exit', 'close']): 
+            res = np.random.choice(['Tata', 'Bye'])
+            ex = False
+        
+        ai.TextSpeech(res)
+    print('--- PyChat left the voice chat ---')
