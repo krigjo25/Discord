f12c0741d28985ea4b032bd61767fb49d9dd8a12
krigjo25 <krigjo25@gmail.com>
2022-06-04 18:55:07 +0200

    Update sampDocumentations.py

--- pyChat/pylib/documentations/sampDocumentations.py
+++ pyChat/pylib/documentations/sampDocumentations.py
@@ -7,6 +7,9 @@ class SampFAQ():
     def __init__(self):
         pass
 
+    def GeneralSAMPQuestions(Question):
+        pass
+
     def CommonSAMPErrors(question):
 
          #   Initializing lists
