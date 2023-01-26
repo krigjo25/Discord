e1a7ad4f6c7b5dcd6064f2c53723d9dc8375ffac
krigjo25 <krigjo25@gmail.com>
2023-01-26 08:05:38 +0100

    Update tables.sql

--- discordDatabases/tables.sql
+++ discordDatabases/tables.sql
@@ -5,8 +5,13 @@ CREATE OR REPLACE TABLE abcent (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
                                 abcent VARCHAR(255) NOT NULL,
 
 
-
 CREATE OR REPLACE TABLE  afkMessages (
                                         id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
                                         memberName VARCHAR(255) NOT NULL,
                                         afkReason VARCHAR(255));
+#   Jumble Game
+CREATE OR REPLACE TABLE waltdisney (
+                                        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                                        characters VARCHAR(255) NOT NULL,
+                                        roles VARCHAR(255) NOT NULL,
+                                        animationname VARCHAR(255) NOT NULL);
